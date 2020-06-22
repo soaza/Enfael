fdc2ecbc8ecdf8fd1d459fcfc9f98a499c10edf5
soaza <65228562+soaza@users.noreply.github.com>
2020-06-22 19:51:35 +0800

    Update Admin Channel.py
    Updated with firebase storage

--- Admin Channel.py
+++ Admin Channel.py
@@ -1,8 +1,9 @@
+from firebase import firebase
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, PicklePersistence)
- 
+
 BCtoken = '1192172658:AAF3I7jtSUySlqSBH5b0kYOiplLmzgwZqqA'
- 
+firebase = firebase.FirebaseApplication('https://orbital-2020.firebaseio.com/admin-channel')
 # To enable logging
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -10,20 +11,21 @@ logger = logging.getLogger(__name__)
 
 # Welcome message with a list of commands
 def start(update, context):
-    update.message.reply_text('Hello! I am the NUS Blind Cupid Bot! ' \
+     update.message.reply_text('Hello! I am the NUS Blind Cupid Bot! ' \
         'I can help you set up a love profile for you to advertise yourself and hopefully find a match!' \
             '\n\nYou can control me by sending me these commands.' \
                 '\n\n/setup - to set up your profile\n/myprofile - to see your profile' \
                     '\n/publicise - to publicise your profile')
- 
+
+
 # States of dictionary of methods
 CHOICES, REPLIES = range(2)
- 
+
 # The keyboard choices to edit your profile
 reply_keyboard = [['Gender', 'Age', 'Race'], ['Height', 'Weight', 'Appearance'], \
     ['Year', 'Major', 'Faculty',], ['Trait 1', 'Trait 2', 'Ideal Match'], ['DONE']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
- 
+
 # To record the facts given in the bot
 def record_facts(user_data):
     facts = list()
@@ -56,7 +58,7 @@ def choices(update, context):
         reply = 'Your {}? Yes, I would love to hear about that!'.format(text.lower())
     update.message.reply_text(reply)
     return REPLIES
- 
+
 # The information the user gave so far
 def information(update, context):
     user_data = context.user_data
@@ -76,6 +78,9 @@ def myprofile(update, context):
 # To exit the conversation handling of setting up your love profile
 def done(update, context):
     user_data = context.user_data
+    uid = update.message.from_user.username
+    #update/store data on firebase
+    result = firebase.put('Users',uid,user_data)
     if 'choice' in user_data:
         del user_data['choice']
     update.message.reply_text("This is your love profile so far:"
@@ -93,22 +98,22 @@ def main():
     pp = PicklePersistence(filename='UserData')
     updater = Updater(BCtoken, persistence = pp, use_context=True)
     dp = updater.dispatcher
- 
+
     # Handles the start method for first time users
     dp.add_handler(CommandHandler('start', start))
- 
+
     # Converse Method to handle the love profile
     conv_handler = ConversationHandler(
         # First Collection: The command to enter the conversation which is SETUP
         entry_points = [CommandHandler('setup', setup)],
- 
+
         # Second Collection: The different conversation methods which comprises of a DICT of CHOICES and REPLIES only
         states = {
             CHOICES: [MessageHandler(Filters.regex('^(Gender|Age|Race|Height|Weight|Appearance|Year|Major|Faculty' \
                 '|Trait 1|Trait 2|Ideal Match|)$'), choices),],
             REPLIES: [MessageHandler(Filters.text, information),],
         },
- 
+
         # Third Collection: The command to exit the conversation
         fallbacks = [MessageHandler(Filters.regex('^DONE$'), done)],
         name = "SetupData",
@@ -116,18 +121,18 @@ def main():
     )
     # To add the conversation handler above
     dp.add_handler(conv_handler)
- 
+
     # To add the handler for user to see their existing love profile
     dp.add_handler(CommandHandler('myprofile', myprofile))
 
     # log all errors
     dp.add_error_handler(error)
- 
+
     # Start the bot
     updater.start_polling()
- 
+
     # To stop the Bot by pressing Ctrl+C or sending a signal to the Bot process
     updater.idle()
- 
+
 if __name__=='__main__':
     main()
